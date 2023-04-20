fb1ae49906b3b0e00c573aa09f425641dbf2bc3b
Mohan Pathapata <mpathapata@contractor.solenis.com>
2023-04-20 22:51:59 +0530

    initital commit

---
+++ app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule as ngRouterModule } from '@angular/router';
+import { CoreModule, BootstrapComponent, RouterModule } from '@c8y/ngx-components';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(),
+    ngRouterModule.forRoot([], { enableTracing: false, useHash: true }),
+    CoreModule.forRoot()
+  ],
+  bootstrap: [BootstrapComponent]
+})
+export class AppModule {}
